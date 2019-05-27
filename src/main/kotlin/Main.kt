8a35f3dcb454f849ba5a4f490bb9faa342411d08
Shashi prakash <shaship359@gmail.com>
2019-05-27 22:44:53 +0530

    You can edit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,30 @@
+fun main (args : Array<String>){
+
+    println("Hello World")
+
+    var shashi: String
+    shashi = "shashi Prakash"
+    println(shashi)
+
+
+    var shashisalary : Int = 32
+    var monthlysalary : Int = shashisalary * 4
+
+    println(shashisalary)
+    println(monthlysalary)
+
+    println()
+
+    val apples: Int = 8
+    val oranges: Int= 2
+    val fruit: Int= apples - oranges
+
+    println(fruit)
+
+    println()
+
+    val week: Int= 130
+    val  years: Double = week/52.0
+    println(years)
+
+}
